c5c308b74d0fe48c8e6fe3507d536d253a239fab
ahmed <ahmed@karriera.de>
2023-12-02 19:39:08 +0100

    Minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
 import { Flex, Text, Button, Link } from "@radix-ui/themes";
 
 export default function Home() {
-  return (
-    <main>
-      <Flex direction="row" gap="5" mt="5">
-        <Link>Hello</Link>
-        <Button variant="soft">Let's go</Button>
-      </Flex>
-    </main>
-  );
+  return <main>Empty page</main>;
 }
